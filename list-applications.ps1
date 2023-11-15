eb37c2a1b9db4b74ccb17696313b143afe5d8abf
emdeh <emdeh-cyber@proton.me>
2023-11-16 09:45:35 +1100

    new scripts from ASD E8 material

---
+++ list-applications.ps1
@@ -0,0 +1,21 @@
+# output a list of installed applications with registered uninstall functionality
+# from ASD Essential Eight material
+
+function Analyze( $p, $f) {
+    Get-ItemProperty $p |foreach {
+        if (($_.DisplayName) -or ($_.version)) {
+            [PSCustomObject]@{
+                From = $f;
+                Name = $_.DisplayName;
+                Version = $_.DisplayVersion;
+                Install = $_.InstallDate
+            }
+        }
+    }
+}
+$s = @()
+$s += Analyze ‘HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*’ 64
+$s += Analyze ‘HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\
+Uninstall\*’ 32
+$s | Sort-Object -Property Name
+
